ff33f1e21f3f8ed39424ceeea7758b1e7a6ebaac
vishnu <vishnu@example.com>
2019-08-13 23:13:04 +0530

    fifth

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget{
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Recipie Book Model',
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr,
+        ),
+        titleSpacing: 80,
+      ),
+      body: Material(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child:
+          Text(
+          'hello vishnu',
+          textAlign: TextAlign.right,
+          style: TextStyle(fontSize: 20,),
+        ),
+      ),
+      ),
+      backgroundColor: Colors.white,
     );
   }
-
 }
